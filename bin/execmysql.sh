5a12f26cc48f5f69b28556cdf4f59294917b3e2d
Kohki Makimoto <kohki.makimoto@gmail.com>
2012-01-05 17:46:17 +0900

    bug fix. modified directory path

--- bin/execmysql.sh
+++ bin/execmysql.sh
@@ -5,8 +5,8 @@ MYSQL_CMD="mysql -uyouruser -pyourpassword -h127.0.0.1 databasename --default-ch
 
 # Initialize process
 CMD_DIR=$(cd $(dirname $0);pwd)
-SQL_DIR=${CMD_DIR}/sql
-OUTPUT_DIR=${CMD_DIR}/output
+SQL_DIR=${CMD_DIR}/../sql
+OUTPUT_DIR=${CMD_DIR}/../output
 
 # Execute sql
 find ${SQL_DIR} -name '*.sql' | while read sql; do
